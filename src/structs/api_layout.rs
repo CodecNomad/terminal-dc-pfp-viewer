256660d6aa1047797a91a05f50452e3ba8c162ee
codecnomad <100487345+CodecNomad@users.noreply.github.com>
2026-09-15 00:14:38 +0200

    Fixed broken json schema

--- src/structs/api_layout.rs
+++ src/structs/api_layout.rs
@@ -1,66 +1,47 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct User {
-    pub _id: String,
-
-    pub _username: String,
-
-    pub _discriminator: String,
-
-    pub _global_name: String,
-
-    pub _display_name: String,
-
-    pub avatar: Avatar,
-
-    pub _has_custom_avatar: bool,
-
-    pub _banner: Option<serde_json::Value>,
-
-    pub _accent_color: Option<serde_json::Value>,
-
-    pub _banner_color: Option<serde_json::Value>,
-
-    pub _public_flags: i64,
-
-    pub _has_public_badge_data: bool,
-
-    pub _public_badge_labels: Vec<serde_json::Value>,
-
-    pub _created_at: String,
-
-    pub _mention: String,
-
-    pub _is_bot: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct Avatar {
-    pub _preview_url: String,
-
-    pub full_size_url: String,
-
-    pub _detected_format: String,
-
-    pub _available_formats: Vec<serde_json::Value>,
-
-    pub _is_animated: bool,
-
-    pub _size_options: Vec<_SizeOptions>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct _SizeOptions {
-    pub _size: i64,
-    pub _url: String,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Root {
     pub user: User,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: String,
+    pub username: String,
+    pub discriminator: String,
+    pub global_name: Value,
+    pub display_name: String,
+    pub avatar: Avatar,
+    pub has_custom_avatar: bool,
+    pub banner: Value,
+    pub accent_color: Value,
+    pub banner_color: Value,
+    pub public_flags: i64,
+    pub has_public_badge_data: bool,
+    pub public_badge_labels: Vec<Value>,
+    pub created_at: String,
+    pub mention: String,
+    pub is_bot: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Avatar {
+    pub preview_url: String,
+    pub full_size_url: String,
+    pub detected_format: String,
+    pub available_formats: Vec<String>,
+    pub is_animated: bool,
+    pub size_options: Vec<SizeOption>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SizeOption {
+    pub size: i64,
+    pub url: String,
 }
